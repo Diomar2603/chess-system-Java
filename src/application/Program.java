5dafe96f4173fcac0cd391503647c247b84ad13f
Leandro Diomar Gomes da Silva <leandrodiomar123@gmail.com>
2020-07-13 20:14:54 -0300

    Tabuleiro de xadrez / UI/ Board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Get ready to the chess!!!");
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
