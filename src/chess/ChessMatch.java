ecb5dfb054a7b1351e1d6dfb451b542050f32471
Leandro Diomar Gomes da Silva <leandrodiomar123@gmail.com>
2020-07-14 18:45:47 -0300

    Transformar posições em coordenadas de xadrez (a1-h8)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,29 +27,26 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column,row).toPosition());
+	}
+	
+	
 	private void initialSetup() {
 		
 		//linha de tras das peças brancas//
 		
-		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
-		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 7));
-		board.placePiece(new King(board, Color.WHITE), new Position(0, 3));
-		board.placePiece(new Queen(board, Color.WHITE), new Position(0, 4));
-		board.placePiece(new Bishop(board, Color.WHITE), new Position(0, 2));
-		board.placePiece(new Bishop(board, Color.WHITE), new Position(0, 5));
-		board.placePiece(new Knight(board, Color.WHITE), new Position(0, 1));
-		board.placePiece(new Knight(board, Color.WHITE), new Position(0, 6));
+		placeNewPiece('a',1,new Rook(board, Color.WHITE));
+		placeNewPiece('h',1,new Rook(board, Color.WHITE));
+		placeNewPiece('e',1,new King(board, Color.WHITE));
+		placeNewPiece('d',1,new Queen(board, Color.WHITE));
+		placeNewPiece('c',1,new Bishop(board, Color.WHITE));
+		placeNewPiece('f',1,new Bishop(board, Color.WHITE));
+		placeNewPiece('g',1,new Knight(board, Color.WHITE));
+		placeNewPiece('b',1,new Knight(board, Color.WHITE));
 		
 		//linha de tras das peças pretas//
-		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 0));
-		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 7));
-		board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
-		board.placePiece(new Queen(board, Color.BLACK), new Position(7, 3));
-		board.placePiece(new Bishop(board, Color.BLACK), new Position(7, 2));
-		board.placePiece(new Bishop(board,Color.BLACK), new Position(7, 5));
-		board.placePiece(new Knight(board, Color.BLACK), new Position(7, 1));
-		board.placePiece(new Knight(board, Color.BLACK), new Position(7, 6));
-		
+	
 		
 		
 	}
