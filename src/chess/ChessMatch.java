eab7a22f70d8affdbcaf88b39453b12f87ae024e
Leandro Diomar Gomes da Silva <leandrodiomar123@gmail.com>
2020-07-14 17:51:39 -0300

    Peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,12 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Bishop;
+import chess.pieces.King;
+import chess.pieces.Knight;
+import chess.pieces.Queen;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +14,7 @@ public class ChessMatch {
 
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -19,4 +26,32 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+	
+	private void initialSetup() {
+		
+		//linha de tras das peças brancas//
+		
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 7));
+		board.placePiece(new King(board, Color.WHITE), new Position(0, 3));
+		board.placePiece(new Queen(board, Color.WHITE), new Position(0, 4));
+		board.placePiece(new Bishop(board, Color.WHITE), new Position(0, 2));
+		board.placePiece(new Bishop(board, Color.WHITE), new Position(0, 5));
+		board.placePiece(new Knight(board, Color.WHITE), new Position(0, 1));
+		board.placePiece(new Knight(board, Color.WHITE), new Position(0, 6));
+		
+		//linha de tras das peças pretas//
+		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 0));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 7));
+		board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
+		board.placePiece(new Queen(board, Color.BLACK), new Position(7, 3));
+		board.placePiece(new Bishop(board, Color.BLACK), new Position(7, 2));
+		board.placePiece(new Bishop(board,Color.BLACK), new Position(7, 5));
+		board.placePiece(new Knight(board, Color.BLACK), new Position(7, 1));
+		board.placePiece(new Knight(board, Color.BLACK), new Position(7, 6));
+		
+		
+		
+	}
+	
 }
